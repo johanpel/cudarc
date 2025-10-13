38ea059b2179d3002af1bee6deda827f5ae70369
Johan Peltenburg <johan.peltenburg+code@gmail.com>
2025-10-13 13:56:56 +0200

    Split type definitions from linking

--- build.rs
+++ build.rs
@@ -139,7 +139,7 @@ fn dynamic_linking(major: usize, minor: usize) {
         println!("cargo:rustc-link-search=native={}", path.display());
     }
 
-    #[cfg(feature = "driver")]
+    #[cfg(feature = "link-driver")]
     println!("cargo:rustc-link-lib=dylib=cuda");
     #[cfg(feature = "nccl")]
     println!("cargo:rustc-link-lib=dylib=nccl");
@@ -161,7 +161,7 @@ fn dynamic_linking(major: usize, minor: usize) {
     println!("cargo:rustc-link-lib=dylib=cusolverMg");
     #[cfg(feature = "cudnn")]
     println!("cargo:rustc-link-lib=dylib=cudnn");
-    #[cfg(feature = "runtime")]
+    #[cfg(feature = "link-runtime")]
     println!("cargo:rustc-link-lib=dylib=cudart");
     #[cfg(feature = "cufile")]
     {
